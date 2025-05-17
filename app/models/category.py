e4aec1352c5021022495edd4e481ecae27b08564
fmarinoa <franco.edson.18014@gmail.com>
2025-05-17 16:16:29 -0500

    feat: agregar clase Category con propiedades y métodos para manejar categorías de productos

---
+++ app/models/category.py
@@ -0,0 +1,44 @@
+class Category:
+    def __init__(self, name: str, description: str, products: list = None):
+        self.__name = name
+        self.__description = description
+        self.__products = products if products is not None else []
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+        
+    @property
+    def description(self):
+        return self.__description
+    
+    @description.setter
+    def description(self, description: str):
+        self.__description = description
+        
+    @property
+    def products(self):
+        return self.__products
+    
+    def __str__(self):
+        return (
+            f"Nombre: {self.name}, Descripción: {self.description}, Productos: {len(self.products)}"
+            f" ({', '.join([str(product) for product in self.products])})"
+        )
+    
+    def add_product(self, product):
+        self.__products.append(product)
+        product.category = self
+        
+    def remove_product(self, product):
+        if product in self.__products:
+            self.__products.remove(product)
+            product.category = None
+            
+    def show_info(self):
+        return str(self)
+    
