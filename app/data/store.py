49fe60ff557b6d2ebab98d004db05fbad398fb58
fmarinoa <franco.edson.18014@gmail.com>
2025-05-18 12:49:12 -0500

    feat: inicializar lista de clientes con datos de ejemplo

--- app/data/store.py
+++ app/data/store.py
@@ -1,5 +1,11 @@
+from app.models.client import Client
+
+cliente1 = Client("CLI001", "FRANCO MARIÑO", "987654321", "franco@gmail.com", "Av. La Marina 2512")
+cliente2 = Client("CLI002", "EDSON AQUISE", "963258741", "edson@gmail.com", "Av. La Marina 2531")
+cliente3 = Client("CLI003", "HENRY MONDRAGON", "951357852", "henry@gmail.com", "Av. La Marina 2122")
+main_clients = [cliente1, cliente2, cliente3]
+
 main_products = []
 main_movements = []
-main_clients = []
 main_categories = []
 main_providers = []
